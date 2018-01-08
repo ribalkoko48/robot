5cd512c6e79a91ae2860a822381d201d270860fc
ribalkoko48 <kotara22@rambler.ru>
2018-01-08 23:52:16 +0300

    add circle animate

--- src/components/CircularDiagram/canvasDiagram.js
+++ src/components/CircularDiagram/canvasDiagram.js
@@ -5,92 +5,139 @@ import './canvasStyle.scss'
 class CanvasDiagram extends Component {
 
     static propTypes = {
-        percentage: PropTypes.array.isRequired
+        data: PropTypes.array.isRequired
+    }
+
+    static defaultProps = {
+        diameter: 88, // (int) диаметр круга
+        lineWidth: 8,
+        backGroundCircle_color: 'gray'
+    }
+
+    componentDidMount() {
+        const {diameter, data, lineWidth, backGroundCircle_color} = this.props
+        this._timer = []
+        const canvas = document.createElement('canvas')
+        canvas.setAttribute('width', diameter)
+        canvas.setAttribute('height', diameter)
+        this.canvas = canvas
+        this.getLine(data, lineWidth, backGroundCircle_color)
+        const canvasWrap = document.querySelector('.canvasDiagram')
+        canvasWrap.appendChild(canvas)
     }
 
     componentWillReceiveProps(nextProps) {
-        if (nextProps.percentage !== this.props.percentage) {
-            this.canvas.getContext("2d").clearRect(0, 0, 88, 88)
-        }
+        this._timer.length && this._timer.forEach((id)=> {
+            clearTimeout(id)
+        })
+        this._timer = []
+        const {diameter, data, lineWidth, backGroundCircle_color} = nextProps
+        this.canvas.getContext("2d").clearRect(0, 0, diameter, diameter)
+        this.getLine(data, lineWidth, backGroundCircle_color)
     }
 
     /**
      * конвертирует % в радианы
      * @param {number} percent
      * */
-    getPosititon(percent) {
+    getPosition(percent) {
 
         const factor = Math.PI * 2 / 100
 
         return percent * factor
     }
 
-    getColor(position){
-        switch (position){
-            case 0: {
-              return 'red'
-            }
-            case 1: {
-                return 'green'
-            }
-            case 2: {
-                return 'yellow'
-            }
-            case 3: {
-                return 'blue'
-            }
-            default: {
-                return 'black'
-            }
-        }
-    }
+    /**
+     * суммирет проценты предыдущих отрезков
+     * @param {number} position
+     * @param {array} arr
+     * */
+    getPercentSum(position, arr) {
+        let sum = 0
 
-    getEndArc(position, arr) {
-        let summ = 0
-
-        arr.forEach((percent, i)=>{
-            if (i < position) summ += percent
+        arr.forEach(({percent}, i)=> {
+            if (i < position) sum += percent
         })
 
-        return summ
+        return sum
     }
 
-    getLine(percentage) {
-        // https://jsfiddle.net/FunnyBanana/3f48mz4L/
+    /**
+     * формируется массив с процентами, от общей суммы
+     * @param {array} data
+     * */
+    getPercentData(data) {
+        let sum = 0
+        data.forEach(obj => sum += obj.value)
 
-        let canvas = this.canvas
+        return data.map(({value, active, color}) => {
+            return {percent: value / sum * 100, active, color}
+        })
+    }
+
+    /**
+     * подготовка данных к отрисовки отрезков
+     * @param {array} data
+     * @param {number} lineWidth
+     * @param {string} backGroundCircle_color
+     * */
+    getLine(data, lineWidth, backGroundCircle_color) {
+        // https://jsfiddle.net/FunnyBanana/3f48mz4L/
+        const percentData = this.getPercentData(data)
+
         let lineStyle = {
-            color: 'gray',
-            lineWidth: 8
+            color: backGroundCircle_color,
+            lineWidth: lineWidth
         }
         let lineWey = {
             startArc: 0,
             endArc: Math.PI * 2
         }
 
-        this.drow(canvas, lineWey, lineStyle)
+        // отрисовка backGroundCircle_color
+        this.draw(lineWey, lineStyle)
 
-        percentage.forEach((_procent, i, arr) => {
-            const percentSumm = this.getEndArc(i ,arr)
+        // отрисовка отрезков в зависимости от наполненности data
+        percentData.forEach(({percent, active, color}, i, arr) => {
+            const percentSum = this.getPercentSum(i, arr)
+            const endPercent = percent - 6
+            let time = 50
 
-            lineStyle.color = this.getColor(i)
-            lineStyle.lineWidth = 4
-            lineWey.startArc = this.getPosititon(percentSumm)
-            lineWey.endArc = this.getPosititon(percentSumm + _procent)
+            for (let i = percentSum; i < percentSum + endPercent; i += 1) {
 
-            this.drow(canvas, lineWey, lineStyle)
+                this._timer.push(setTimeout(()=> {
+                    lineStyle.color = color
+                    lineStyle.lineWidth = active ? lineWidth : lineWidth * 0.6
+
+                    lineWey.startArc = this.getPosition(i)
+                    lineWey.endArc = this.getPosition(i + 1)
+
+                    this.draw(lineWey, lineStyle)
+                }, time))
+
+                time += 5
+            }
         })
-
     }
 
-    drow(canvas, lineWey, lineStyle) {
+    /**
+     * рисуем отрезок круга
+     * @param {object} lineWey
+     * @param {object} lineStyle
+     * */
+    draw = (lineWey, lineStyle) => {
+        const {diameter} = this.props
+        const canvas = this.canvas
+        const width = diameter / 2
+        const height = diameter / 2
+        const radius = width / 2
         const context = canvas.getContext("2d");
         const {startArc, endArc} = lineWey
         const {color, lineWidth} = lineStyle
 
         context.beginPath();
 
-        context.arc(44, 44, 22, startArc, endArc, false);
+        context.arc(width, height, radius, startArc, endArc, false);
         context.lineWidth = lineWidth
         context.strokeStyle = color;
         context.lineCap = "round";
@@ -98,17 +145,8 @@ class CanvasDiagram extends Component {
     }
 
     render() {
-        const percentage = this.props.percentage
-        const width = 80;
-        const lineWidth = width / 10
-        const size = width + lineWidth
-        const styleObj = {"width": size + 'px', "height": size + 'px', "fontSize": Math.floor((width - lineWidth) / 4) + 'px'}
-        setTimeout(() => this.getLine(percentage, lineWidth, width), 0)
-
         return (
-            <div className="canvasDiagram" style={styleObj}>
-                <canvas ref={(can) => this.canvas = can} width={88} height={88} />
-            </div>
+            <div className="canvasDiagram"></div>
         )
     }
 
--- src/components/CircularDiagram/index.js
+++ src/components/CircularDiagram/index.js
@@ -12,20 +12,24 @@ class CircularDiagram extends Component {
         percentage: 0
     }
 
-    detPercentageArr(){
-        switch (this.state.percentage){
+    detPercentageArr() {
+        switch (this.state.percentage) {
 
-            case 1 : {
-                return [20, 40, 40]
+            case 1 :
+            {
+                return [{active: false, value: 350, color: 'red'}, {active: false, value: 150, color: 'blue'}, {active: true, value: 500, color: 'yellow'}]
             }
-            case 2 : {
-                return [30, 60, 10]
+            case 2 :
+            {
+                return [{active: true, value: 100, color: 'green'}, {active: false, value: 200, color: 'pink'}, {active: false, value: 700, color: 'red'}]
             }
-            case 3 : {
-                return [10, 10, 80]
+            case 3 :
+            {
+                return [{active: false, value: 600, color: 'black'}, {active: true, value: 200, color: 'green'}, {active: false, value: 200, color: 'red'}]
             }
-            default: {
-                return [33.33, 33.33, 33.33]
+            default:
+            {
+                return [{active: false, value: 333, color: 'red'}, {active: false, value: 333, color: 'blue'}, {active: false, value: 333, color: 'yellow'}, {active: false, value: 333, color: 'black'}, {active: false, value: 333, color: 'pink'}]
             }
         }
     }
@@ -35,11 +39,8 @@ class CircularDiagram extends Component {
         return (<div>
                 <div onClick={() => this.setState({percentage: this.state.percentage + 1})}>добавляем 10</div>
                 <div onClick={() => this.setState({percentage: this.state.percentage - 1})}>убрать 10</div>
-                <div style={{width: '100px'}}>
-                   {/* <CircularProgressbar percentage={this.detPercentageArr()} text="%" textNumber={650}/>*/}
-                    <CanvasDiagram percentage={this.detPercentageArr()} />
-
-                </div>
+               
+                <CanvasDiagram data={this.detPercentageArr()}/>
             </div>
         )
     }
